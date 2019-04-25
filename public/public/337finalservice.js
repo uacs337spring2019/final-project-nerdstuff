6c4423bcceb33be38e5a608a4bed262df0ff9d7b
jlockett130 <49957400+jlockett130@users.noreply.github.com>
2019-04-24 21:51:36 -0700

    Add files via upload

---
+++ public/public/337finalservice.js
@@ -0,0 +1,40 @@
+/*
+Jake Lockett & Jacob Sommer
+CSC 337
+4/24/2019
+337finalservice.js
+*/
+const express = require("express");
+const app = express();
+const fs = require('fs');
+app.use(express.static('public'));
+console.log('web service started');
+app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  let type=req.query.type;
+  let read;
+  let path;
+  let data={};
+  if (type=="race") {
+    path="races.txt";
+    read=fs.readFileSync(path,"utf8");
+    read=read.split("\n");
+    i=Math.floor(Math.random() * read.length);
+    res.send(JSON.stringify(read[i]));
+  }
+  if (type=="class") {
+    path="classes.txt";
+    read=fs.readFileSync(path,"utf8");
+    read=read.split("\n");
+    i=Math.floor(Math.random() * read.length);
+    res.send(JSON.stringify(read[i]));
+  }
+  if (type=="background") {
+    path="xbackgrounds.txt";
+    read=fs.readFileSync(path,"utf8");
+    read=read.split("\n");
+    i=Math.floor(Math.random() * read.length);
+    res.send(JSON.stringify(read[i]));
+  }
+})
+app.listen(process.env.PORT);
